74e91d14b5a6cc31c993530b74c080023f490b73
Nininanou16 <paul.peronredon@skypvp.fr>
2022-11-06 16:47:45 +0100

    New transmission system

---
+++ Buttons/Transmission.js
@@ -0,0 +1,31 @@
+const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+
+module.exports = async (Client, interaction) => {
+    let modal = new ModalBuilder()
+        .setCustomId("Transmission")
+        .setTitle("Transmission");
+
+    let prob = new TextInputBuilder()
+        .setCustomId("problematic")
+        .setLabel('Problématique de l\'écoute :')
+        .setStyle(TextInputStyle.Paragraph);
+
+    let impressions = new TextInputBuilder()
+        .setCustomId("impressions")
+        .setLabel('Impressions sur l\'écoute :')
+        .setStyle(TextInputStyle.Paragraph);
+
+    let sup = new TextInputBuilder()
+        .setCustomId('suplement')
+        .setLabel('Informations supplémentaires :')
+        .setStyle(TextInputStyle.Paragraph)
+        .setRequired(false);
+
+    let probRow = new ActionRowBuilder().addComponents(prob);
+    let impressionsRow = new ActionRowBuilder().addComponents(impressions);
+    let supRow = new ActionRowBuilder().addComponents(sup);
+
+    modal.addComponents(probRow, impressionsRow, supRow);
+
+    await interaction.showModal(modal);
+}
